edffb2f837d24e6c3ed34fb3bb49a7e9a33f50e5
StefanBergheimer <stefanbergheimer@gmail.com>
2018-02-09 20:48:30 +0100

    New output: Energy offers and reserve offers per Island. Reserves split into FIR and SIR.

--- app.R
+++ app.R
@@ -5,39 +5,74 @@ library(RCurl)
 library(shiny)
 library(DT)
 library(stringr)
+library(lubridate)
+library(plotly)
 
+# load helper functions
+source("helper.R")
 
 # user interface
 ui <- fluidPage(
-  sidebarPanel(
-    
-    # Date Input + Action button
-    dateInput(inputId = "date",
-              label = "Date",
-              value = "2017-12-01",
-              min = "2013-01-01",
-              max = "2017-12-31"),
-    actionButton(inputId = "update_date", "Load data"),
-    
-    # Trading Period
-    numericInput(inputId = "tp",
-                 label = "Trading Period",
-                 min = 1,
-                 max = 48,
-                 step = 1,
-                 value = 1),
-    
-    # Trader
-    selectInput(inputId = "trader",
-                label = "Trader", 
-                choices = c("Contact Energy" = "CTCT",
-                            "Trust Energy" = "TRUS"))
-  ),
   
-  mainPanel(
-    #DT::dataTableOutput(outputId = "head_DT")
-    plotOutput(outputId = "step_offers_energy"),
-    plotOutput(outputId = "step_offers_reserves")
+  tabsetPanel(
+    
+    tabPanel(title = "Intro"),
+    
+    tabPanel(title = "Plots",
+             
+      sidebarPanel(width = 2,
+        
+        wellPanel(
+          # Date Input + Action button
+          dateInput(inputId = "date",
+                  label = "Date",
+                  value = "2017-12-01",
+                  min = "2013-01-01",
+                  max = "2017-12-31"),
+          actionButton(inputId = "update_date", "Load data")
+        ),
+        
+        br(),
+        
+        wellPanel(
+        # Trading Period
+          numericInput(inputId = "tp",
+                       label = "Select trading period",
+                       min = 1,
+                       max = 48,
+                       step = 1,
+                       value = 1),
+          
+          # Trader
+          selectInput(inputId = "trader",
+                      label = "Select trader", 
+                      choices = c("Contact Energy" = "CTCT",
+                                  "Trust Energy" = "TRUS",
+                                  "Mighty River Power" = "MRPL",
+                                  "Genesis" = "GENE",
+                                  "Meridian" = "MERI")),
+          
+          # Predispatch
+          checkboxGroupInput(inputId = "predispatch",
+                      label = "Select predispatch",
+                      choices = c("First predispatch", "Last predispatch"),
+                      selected = c("First predispatch", "Last predispatch"))
+        )
+      ),
+    
+      mainPanel(width = 10,
+        splitLayout(
+          plotlyOutput(outputId = "energy_offer_NI"),
+          plotlyOutput(outputId = "energy_offer_SI")
+        ),
+        splitLayout(
+          plotlyOutput(outputId = "reserve_offer_NI_FIR"),
+          plotlyOutput(outputId = "reserve_offer_NI_SIR"),
+          plotlyOutput(outputId = "reserve_offer_SI_FIR"),
+          plotlyOutput(outputId = "reserve_offer_SI_SIR")
+        )
+      )
+    )
   )
 )
 
@@ -51,31 +86,106 @@ server <- function(input, output){
     read_csv(filename)
   }, ignoreNULL = TRUE)
   
-  # header of data table
-  #output$head_DT <- DT::renderDataTable({
-  #  DT::datatable(data = selected_data(), options = list(pageLength = 10))
-  #})
-  
   # create plot for energy offers
-  output$step_offers_energy <- renderPlot({
-    filter(selected_data(), Trader == input$trader, ProductType == "Energy",
-           TradingPeriod == input$tp, IsLatest == "Y") %>%
-      arrange(DollarsPerMegawattHour) %>%
-      mutate(cumMegawatt = cumsum(Megawatt)) %>%
-      ggplot(aes(x = cumMegawatt, y = DollarsPerMegawattHour)) +
-      geom_step(direction = "vh") +
-      expand_limits(x = 0, y = 0)
+  offer_data <- reactive({
+    filter(selected_data(), Trader == input$trader, TradingPeriod == input$tp) %>%
+            left_join(mapping_island, by = c("PointOfConnection" = "POC code")) %>%
+            mutate(SubmissionDate = ymd_hms(paste0(UTCSubmissionDate, UTCSubmissionTime)),
+                   TradingDateTime = ymd_hm(paste0(TradingDate, tp_to_hour(TradingPeriod)))) %>%
+            mutate(TimeDiff = difftime(SubmissionDate, TradingDateTime,  unit = "hours") ) %>%
+            mutate(Predispatch = timediff_to_predispatch(TimeDiff, TradingPeriod)) 
+  })
+  
+  output$energy_offer_NI <- renderPlotly({
+    temp <- filter(offer_data(), ProductType == "Energy", Island == "NI")
+    
+    data_first_predispatch <- get_first_predispatch(temp)
+      
+    data_last_predispatch <- get_last_predispatch(temp)
+      
+    p <- ggplot() +
+        geom_step(data = data_first_predispatch, aes(x = cumMegawatt, y = DollarsPerMegawattHour), linetype = "dashed", direction = "vh") +
+        geom_step(data = data_last_predispatch, aes(x = cumMegawatt, y = DollarsPerMegawattHour), direction = "vh") +
+        expand_limits(x = 0, y = 0) 
+    
+    ggplotly(p)
+  })
+  
+  output$energy_offer_SI <- renderPlotly({
+    temp <- filter(offer_data(), ProductType == "Energy", Island == "SI")
+    
+    data_first_predispatch <- get_first_predispatch(temp)
+    
+    data_last_predispatch <- get_last_predispatch(temp)
+    
+    p <- ggplot() +
+      geom_step(data = data_first_predispatch, aes(x = cumMegawatt, y = DollarsPerMegawattHour), linetype = "dashed", direction = "vh") +
+      geom_step(data = data_last_predispatch, aes(x = cumMegawatt, y = DollarsPerMegawattHour), direction = "vh") +
+      expand_limits(x = 0, y = 0) 
+    
+    ggplotly(p)
   })
 
   # create plot for reserves
-  output$step_offers_reserves <- renderPlot({
-    filter(selected_data(), Trader == input$trader, ProductType == "Reserve",
-           TradingPeriod == input$tp, IsLatest == "Y") %>%
-      arrange(DollarsPerMegawattHour) %>%
-      mutate(cumMegawatt = cumsum(Megawatt)) %>%
-      ggplot(aes(x = cumMegawatt, y = DollarsPerMegawattHour)) +
-      geom_step(direction = "vh") +
+  output$reserve_offer_NI_FIR <- renderPlotly({
+    temp <- filter(offer_data(), ProductType == "Reserve", ProductSubClass == "FIR", Island == "NI") 
+      
+    data_first_predispatch_reserve <- get_first_predispatch(temp)
+      
+    data_last_predispatch_reserve <- get_last_predispatch(temp)
+      
+    q <- ggplot() +
+        geom_step(data = data_first_predispatch_reserve, aes(x = cumMegawatt, y = DollarsPerMegawattHour), linetype = "dashed", direction = "vh") +
+        geom_step(data = data_last_predispatch_reserve, aes(x = cumMegawatt, y = DollarsPerMegawattHour), direction = "vh") +
+        expand_limits(x = 0, y = 0)
+    
+    ggplotly(q)
+  })
+  
+  output$reserve_offer_NI_SIR <- renderPlotly({
+    temp <- filter(offer_data(), ProductType == "Reserve", ProductSubClass == "SIR", Island == "NI") 
+    
+    data_first_predispatch_reserve <- get_first_predispatch(temp)
+    
+    data_last_predispatch_reserve <- get_last_predispatch(temp)
+    
+    q <- ggplot() +
+      geom_step(data = data_first_predispatch_reserve, aes(x = cumMegawatt, y = DollarsPerMegawattHour), linetype = "dashed", direction = "vh") +
+      geom_step(data = data_last_predispatch_reserve, aes(x = cumMegawatt, y = DollarsPerMegawattHour), direction = "vh") +
       expand_limits(x = 0, y = 0)
+    
+    ggplotly(q)
+  })
+  
+  # create plot for reserves
+  output$reserve_offer_SI_FIR <- renderPlotly({
+    temp <- filter(offer_data(), ProductType == "Reserve", ProductSubClass == "FIR", Island == "SI") 
+    
+    data_first_predispatch_reserve <- get_first_predispatch(temp)
+    
+    data_last_predispatch_reserve <- get_last_predispatch(temp)
+    
+    q <- ggplot() +
+      geom_step(data = data_first_predispatch_reserve, aes(x = cumMegawatt, y = DollarsPerMegawattHour), linetype = "dashed", direction = "vh") +
+      geom_step(data = data_last_predispatch_reserve, aes(x = cumMegawatt, y = DollarsPerMegawattHour), direction = "vh") +
+      expand_limits(x = 0, y = 0)
+    
+    ggplotly(q)
+  })
+  
+  output$reserve_offer_SI_SIR <- renderPlotly({
+    temp <- filter(offer_data(), ProductType == "Reserve", ProductSubClass == "SIR", Island == "SI") 
+    
+    data_first_predispatch_reserve <- get_first_predispatch(temp)
+    
+    data_last_predispatch_reserve <- get_last_predispatch(temp)
+    
+    q <- ggplot() +
+      geom_step(data = data_first_predispatch_reserve, aes(x = cumMegawatt, y = DollarsPerMegawattHour), linetype = "dashed", direction = "vh") +
+      geom_step(data = data_last_predispatch_reserve, aes(x = cumMegawatt, y = DollarsPerMegawattHour), direction = "vh") +
+      expand_limits(x = 0, y = 0)
+    
+    ggplotly(q)
   })
 }
 
--- helper.R
+++ helper.R
@@ -1,7 +1,56 @@
-# Helper function
+
+### Helper functions
+
+# Trading Period to Hour
 tp_to_hour <- function(tp){
   vector_with_hour_min <- format( seq.POSIXt(as.POSIXct(Sys.Date()), as.POSIXct(Sys.Date()+1), by = "30 min"),
                                   "%H%M", tz="GMT")
   time_string <- vector_with_hour_min[tp]
   return(time_string)
+}
+
+# DiffTime to predispatch (takes in TimeDiff and TradingPeriod and returns predispatch number)
+timediff_to_predispatch <- function(TimeDiff, TradingPeriod = 1){
+  Predispatch <- ifelse(TradingPeriod %in% c(seq(1, 45, by = 4)), cut(as.numeric(TimeDiff), breaks = c(seq(0, -3.5, -0.5), seq(-4, -34, -2), -5000)), 
+                   ifelse(TradingPeriod %in% c(seq(2, 46, by = 4)), cut(as.numeric(TimeDiff), breaks = c(seq(0, -3.5, -0.5), seq(-4.5, -34.5, -2), -5000)), 
+                     ifelse(TradingPeriod %in% c(seq(3, 47, by = 4)), cut(as.numeric(TimeDiff), breaks = c(seq(0, -3.5, -0.5), seq(-5, -35, -2), -5000)), 
+                       ifelse(TradingPeriod %in% c(seq(4, 48, by = 4)), cut(as.numeric(TimeDiff), breaks = c(seq(0, -3.5, -0.5), seq(-5.5, -35.5, -2), -5000)), NA) )))
+  return(Predispatch)
+}
+
+# function that selects data for the graph of the first predispatch
+get_first_predispatch <- function(data){
+  # Takes in dataframe of one trader's offer data and returns dataframe with data to plot his supply curve submitted before the first predispatch
+  fp <- filter(data, Predispatch == 1) %>%
+    group_by(Island, PointOfConnection, Unit, ProductType, ProductSubClass, Band) %>%
+    arrange(SubmissionOrder) %>%
+    slice(n()) %>%
+    ungroup() %>%
+    group_by(Island, ProductType, ProductSubClass) %>%
+    arrange(DollarsPerMegawattHour) %>%
+    mutate(cumMegawatt = cumsum(Megawatt)) %>%
+    ungroup() %>%
+    select(DollarsPerMegawattHour, cumMegawatt, Island) 
+  
+  fp <- add_row(fp, cumMegawatt = 0, DollarsPerMegawattHour = min(fp$DollarsPerMegawattHour)) %>%
+    arrange(cumMegawatt)
+  
+  return(fp)
+}
+
+
+# function that selects data for the graph of the first predispatch
+get_last_predispatch <- function(data){
+  # Takes in dataframe of one trader's offer data and returns dataframe with data to plot his supply curve submitted before the last predispatch
+  lp <- filter(data, IsLatest == "Y") %>%
+    group_by(Island, ProductType, ProductSubClass) %>%
+    arrange(DollarsPerMegawattHour) %>%
+    mutate(cumMegawatt = cumsum(Megawatt)) %>%
+    ungroup() %>%
+    select(DollarsPerMegawattHour, cumMegawatt, Island)
+
+  lp <- add_row(lp, cumMegawatt = 0, DollarsPerMegawattHour = min(lp$DollarsPerMegawattHour)) %>%
+    arrange(cumMegawatt)
+  
+  return(lp)
 }
